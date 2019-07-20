80dd62effc416736fd98ff7b9922661c95d23609
fonzmp <fonzmp@gmail.com[color]>
2019-07-20 14:11:17 -0500

    added models and constants to work off of

---
+++ client/src/assets/data/DataModel.js
@@ -0,0 +1,19 @@
+export const DataModel = {
+  navigationLinks: [
+    {
+      display: 'Home',
+      href: "/",
+      target: ""
+    },
+    {
+      display: 'Recently Added',
+      href: "",
+      target: ""
+    },
+    {
+      display: 'My Favorites',
+      href: "/favorites",
+      target: ""
+    },
+  ]
+}
