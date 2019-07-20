41b89e43d755faa8354cdfef82176d3a1ccf0811
independnt <gonzalezdennis6@gmail.com>
2019-07-20 17:40:52 -0400

    merge

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import Home from './shared/home'
-<<<<<<< HEAD
-=======
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faBell, faUser } from '@fortawesome/free-solid-svg-icons'
 import NavBar from './shared/navigation/navbar'
 import { ApplicationConstants } from './_constants/application.constants'
->>>>>>> f1dcb5edc6ac842af41db6d4b9eb031f39ddf4af
 import './App.css';
 
 function App() {
-  console.log('process ', process.env.REACT_APP_YOUTUBE_KEY)
   return (
-<<<<<<< HEAD
-    <div className="App">
-      <Home></Home>
-    </div>
-=======
     <Router>
       <NavBar />
       <div>
@@ -31,7 +22,6 @@ function App() {
         </React.Fragment>
       </div>
     </Router>
->>>>>>> f1dcb5edc6ac842af41db6d4b9eb031f39ddf4af
   );
 }
 
