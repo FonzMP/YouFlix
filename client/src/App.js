5c5211f1ae9a7e5b99a8de1cf6078bf379e840fe
independnt <gonzalezdennis6@gmail.com>
2019-07-20 17:22:28 -0400

    Removed App.js nonsense

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
+import Home from './shared/home'
 import './App.css';
 
 function App() {
   console.log('process ', process.env.REACT_APP_YOUTUBE_KEY)
   return (
     <div className="App">
-      {console.log(process.env.REACT_APP_YOUTUBE_KEY)}
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home></Home>
     </div>
   );
 }
