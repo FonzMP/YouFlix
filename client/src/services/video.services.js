7a022bf66599b53c432f67f29acb9355787f483e
Alex Wilson <fonzmp@gmail.com>
2019-07-23 22:53:00 -0400

    cleaned video service

--- client/src/services/video.services.js
+++ client/src/services/video.services.js
@@ -2,18 +2,13 @@ import { ApplicationConstants } from "../_constants/application.constants";
 
 const c = ApplicationConstants;
 
-export const VideoServices = {
+export const videoServices = {
   getYouTubeVideos
 };
 
 function getYouTubeVideos() {
   return fetch(c.YOUTUBE_API + process.env.REACT_APP_YOUTUBE_KEY)
     .then(response => response.json())
-    .then(data => {
-      console.log(
-        "data.videoData",
-        data.items[0].snippet.thumbnails.medium.url
-      );
-      return data;
-    });
+    .then(data => data)
+    .catch(err => console.log("error during getYoutubeVideos"));
 }
--- client/src/shared/home.js
+++ client/src/shared/home.js
@@ -2,8 +2,8 @@ import React from "react";
 import VideoGrid from "../components/video/VideoGrid";
 import PlaceHolder from "../components/images/placeholder";
 import { ApplicationConstants } from "../_constants/application.constants";
-import { VideoServices } from "../services/video.services";
-import { VideoGridStyled } from "../components/video/VideoGrid"
+import { videoServices } from "../services/video.services";
+import { VideoGridStyled } from "../components/video/VideoGrid";
 
 const c = ApplicationConstants;
 export default class Home extends React.Component {
@@ -23,7 +23,7 @@ export default class Home extends React.Component {
   //<iframe width="360" height="215" src="https://www.youtube.com/embed/${VIDEO_ID_INSERT}" frameborder="20" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
   fetchVideos() {
-    return VideoServices.getYouTubeVideos().then(response => {
+    return videoServices.getYouTubeVideos().then(response => {
       this.setState({
         videos: response,
         loading: false
@@ -43,7 +43,9 @@ export default class Home extends React.Component {
         {this.state.loading ? (
           <div>loading.....</div>
         ) : (
-          <VideoGridStyled>{this.state.videos ? this.renderPlaceholders() : null}</VideoGridStyled>
+          <VideoGridStyled>
+            {this.state.videos ? this.renderPlaceholders() : null}
+          </VideoGridStyled>
         )}
       </div>
     );
