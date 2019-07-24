66c6029d638a45b2121cae328eba9dbae2fd0041
Alex Wilson <fonzmp@gmail.com>
2019-07-24 00:26:19 -0400

    added sidebar items to datamodel

--- client/src/assets/data/DataModel.js
+++ client/src/assets/data/DataModel.js
@@ -1,23 +1,45 @@
-import { ApplicationConstants } from '../../_constants/application.constants'
+import { ApplicationConstants } from "../../_constants/application.constants";
 
-const c = ApplicationConstants
+const c = ApplicationConstants;
 
 export const DataModel = {
   navigationLinks: [
     {
-      display: 'Home',
+      display: "Home",
       href: `${c.HOME}`,
       target: ""
     },
     {
-      display: 'Recently Added',
+      display: "Recently Added",
       href: `${c.RECENT}`,
       target: ""
     },
     {
-      display: 'My Favorites',
+      display: "My Favorites",
       href: `${c.FAVORITES}`,
       target: ""
+    }
+  ],
+  sideBarContext: [
+    {
+      display: "Sports",
+      icon: "running"
     },
+    {
+      display: "Music",
+      icon: "music"
+    },
+    {
+      display: "Gaming",
+      icon: "headset"
+    },
+    {
+      display: "Movie",
+      icon: "video"
+    },
+    {
+      display: "Television",
+      icon: "tv"
+    }
   ]
-}
+};
